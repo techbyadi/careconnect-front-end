67b88f8820729e0af2a5d6b7b0119e6e2ec62506
techbyadi <pssharma.aditya@gmail.com>
2024-09-05 09:18:10 -0700

    add landing page and stub myappoitments and newappoitment pages

---
+++ src/pages/AppointmentList/AppointmentList.jsx
@@ -0,0 +1,12 @@
+// css
+import styles from './AppointmentList.module.css'
+
+const AppointmentList = () => {
+  return (
+    < main className={styles.container}>
+      <h1>List Of Appointments</h1>
+    </main>
+  )
+}
+
+export default AppointmentList
---
+++ src/pages/NewAppointment/NewAppointment.jsx
@@ -0,0 +1,12 @@
+// css
+import styles from './NewAppointment.module.css'
+
+const AppointmentList = () => {
+  return (
+    < main className={styles.container}>
+      <h1>Create a new appoitment</h1>
+    </main>
+  )
+}
+
+export default AppointmentList
