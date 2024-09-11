9e335c95cd676e860f2800cb9e7c09066a5e5ebf
Taeyong Sung <canny0201@gmail.com>
2024-09-11 16:48:20 -0400

    update css

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -3,12 +3,14 @@
 import edit from '../../assets/icons/edit.svg'
 import trash from '../../assets/icons/trash.svg'
 import create from '../../assets/icons/create.svg'
+import calendar from '../../assets/icons/calendar.svg'
 
 const Icon = ({ category }) => {
   const icons = {
     Edit: edit,
     Trash: trash,
     Create: create,
+    Calendar: calendar
   }
 
   return (
--- src/components/ReviewCard/ReviewCard.jsx
+++ src/components/ReviewCard/ReviewCard.jsx
@@ -4,40 +4,53 @@ import { NavLink } from 'react-router-dom'
 // css
 import styles from "./ReviewCard.module.css"
 
+// assets
+import profileIcon from '../../assets/icons/profile.png'
+
 // components
 import Icon from '../Icon/Icon'
+import DateCard from '../DateCard/DateCard'
 
 const ReviewCard = (props) => {
   const { 
     review, 
     doctor, 
     currentUser, 
-    handleDeleteReview 
+    handleDeleteReview,
   } = props;
+  
+  const photo = review.author.photo ? review.author.photo : profileIcon
 
   return (
     <main>
       <article className={styles.container}>
         <header>
-          <span>
-            <h4>{review.rating} star out of 5</h4>  
-            <div>
-              <h5>Review by {review.author.name}</h5>
-              {currentUser?.profile === review.author?._id && (
-                <>
-                  <NavLink to='/review/edit' state={{ review, doctor }}>
-                    <Icon category='Edit'/>
-                  </NavLink>
-                  <NavLink 
+          <div>
+            {currentUser?.profile === review.author?._id && (
+              <>
+                <NavLink to='/review/edit' state={{ review, doctor }}>
+                  <Icon category='Edit'/>
+                </NavLink>
+                <NavLink 
                   onClick={() => handleDeleteReview(doctor._id, review._id)} state={{ review, doctor }}>
-                    <Icon category='Trash' />
-                  </NavLink>
-                </>
-              )}
+                  <Icon category='Trash' />
+                </NavLink>
+              </>
+            )}
+          </div>
+          <h3>⭐️ {review.rating} / 5 </h3>  
+          <span>
+            <div>
+              &nbsp;&nbsp;<img src={photo} alt="The user's avatar" />
+              &nbsp;
+              <h5>
+                {review.author.name}
+                <DateCard createdAt={review.createdAt} />
+              </h5>
             </div>
           </span>
         </header>
-        <h5>{review.content}</h5>
+        <h4>{review.content}</h4>
       </article>
     </main>
   );
